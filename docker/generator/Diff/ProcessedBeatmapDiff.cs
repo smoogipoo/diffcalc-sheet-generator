2b66f45cb923ee62352af5edb03c39605e93f8e0
Dan Balasescu <smoogipoo@smgi.me>
2022-07-11 19:20:35 +0900

    Renamespace some files

--- docker/generator/Diff/ProcessedBeatmapDiff.cs
+++ docker/generator/Diff/ProcessedBeatmapDiff.cs
@@ -1,9 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using Generator.Diff;
 using Generator.Models;
 
-namespace Generator;
+namespace Generator.Diff;
 
 public record struct ProcessedBeatmapDiff(BeatmapDiff Diff, Beatmap Beatmap);
--- docker/generator/Diff/ProcessedScoreDiff.cs
+++ docker/generator/Diff/ProcessedScoreDiff.cs
@@ -1,9 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using Generator.Diff;
 using Generator.Models;
 
-namespace Generator;
+namespace Generator.Diff;
 
 public record struct ProcessedScoreDiff(SoloScore Score, ScoreDiff Diff, Beatmap Beatmap);
