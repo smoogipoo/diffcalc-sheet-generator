863ca47903363219bcc419b130150f286b42abed
Dan Balasescu <smoogipoo@smgi.me>
2024-04-15 19:00:03 +0900

    Fix attrib name

--- docker/setup/fixes/2-add-dummy-attributes.sql
+++ docker/setup/fixes/2-add-dummy-attributes.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO `osu_difficulty_attribs` (attrib_id, name)
 (
-    SELECT i, 'Attrib ID ' + i FROM
+    SELECT i, CONCAT('Attrib ID ', i) FROM
     (
         WITH RECURSIVE seq AS (
             SELECT 1 AS i UNION ALL SELECT i + 2 FROM seq WHERE i <= 1000
