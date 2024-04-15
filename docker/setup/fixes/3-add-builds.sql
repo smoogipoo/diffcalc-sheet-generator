9eee32ca6b6a245908e7739af61b134fc08464c7
Dan Balasescu <smoogipoo@smgi.me>
2024-04-15 19:12:18 +0900

    Only add builds for scores that have pp

--- docker/setup/fixes/3-add-builds.sql
+++ docker/setup/fixes/3-add-builds.sql
@@ -1,4 +1,4 @@
 INSERT INTO `osu_builds` (build_id, allow_performance)
 (
-    SELECT DISTINCT(build_id), true FROM `scores` WHERE build_id IS NOT NULL
+    SELECT DISTINCT(build_id), true FROM `scores` WHERE build_id IS NOT NULL AND pp IS NOT NULL
 );
